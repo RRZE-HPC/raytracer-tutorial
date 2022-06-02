b90610697bcf25cbd2d99e1ad881ab34e74a2063
JanLJL <jan.laukemann@fau.de>
2022-06-02 09:13:59 +0200

    added camera in initial state

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "camera.hpp"
 #include "color.hpp"
 #include "imageio.hpp"
 #include "ray.hpp"
@@ -39,6 +40,9 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    // TODO --- TASK 4
+    // const int samples_per_pixel = 100;
+    // -----TASK-4----
     color* image;
     if((image=(vec3*)malloc(image_width * image_height * sizeof(vec3)))==NULL) {
         fprintf(stderr,"Could not allocate picture memory!\n");
@@ -53,14 +57,7 @@ int main() {
     // -----TASK-3----
 
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    camera cam;
 
     // Render
     double ws,we,cs,ce;
@@ -69,9 +66,10 @@ int main() {
     {
         for (int i=0; i<image_width; ++i)
         {
+            // TODO TASK 4
             auto u = double(i) / (image_width-1);
             auto v = double(j) / (image_height-1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            ray r = cam.get_ray(u, v);
             image[j*image_width + i] = ray_color(r);
         }
     }
